7bf4a8578d2895be8376d98f316ca064870fdbd0
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-20 23:25:18 +0500

    Add files via upload

---
+++ JavaScript/Class 01/app.js
@@ -0,0 +1,94 @@
+alert(100);
+
+document.write(50);
+
+console.log(200);
+
+// Data types in JS
+
+// 1) Number
+
+// 2) String
+
+alert("Hello world");
+
+alert(
+  "Lorem ipsum \n dolor sit amet consectetur adipisicing elit. Asperiores, eos."
+);
+
+document.write(
+  "<b><i>Hello world</b></i><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, eos."
+);
+
+console.log("Hello world");
+
+var faraz = 20;
+
+console.log(faraz);
+
+var firstname = "Mr";
+
+var lastname = "John";
+
+var fullname = firstname + " " + lastname;
+
+console.log(fullname);
+
+var firstname = "Muhammad";
+
+var lastname = "Iqbal";
+
+var age = 20;
+
+var email = "faraz@gmail.com";
+
+var institute = "Jawan Pakistan";
+
+var finalResult =
+  "Welcome my name is " +
+  firstname +
+  " " +
+  lastname +
+  " I am " +
+  age +
+  " years old" +
+  "I study in " +
+  institute +
+  "and my email address is " +
+  email;
+
+console.log(finalResult);
+
+console.log(
+  "Welcome my name is muhammad faraz.I am 20 years old. i study in jawan pakistan and my email address is faraz@gmail.com"
+);
+
+// When we create a vairable their are two possible types of variables
+
+// 1) variable declaration
+
+var number;
+
+console.log(number);
+
+// 2) variable declaration and initialization
+
+var number = 10;
+
+var number = 10;
+
+var number2 = 20;
+
+var number = 20;
+
+console.log(number);
+
+// Boolean
+
+// true
+
+// false
+
+var bool = false;
+
+console.log(bool);
