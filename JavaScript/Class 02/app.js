dd2390ef8eacfc061244837eef919d927ea56bb5
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-23 20:48:39 +0500

    Add files via upload

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,176 @@
+// var name1;
+
+// console.log("before", name1);
+
+// name1 = "abc";
+
+// console.log("after", name1);
+
+// alert(name);
+
+// var firstname = "Mr";
+
+// var lastname = "Joe";
+
+// document.write(
+//   "<b>Hello</b> " + "<i>" + firstname + "</i>" + " " + "<i>" + lastname + "</i>"
+// );
+
+// document.write(
+//   "<select><option>faraz</option> <option>hamza</option></select>"
+// );
+
+// var first_name = "ABC";
+
+// var number = "20";
+// var number2 = 10;
+
+// console.log(number * number2);
+
+// var calculation = (2 + 4) * 10;
+
+// var calculation = 5 % 2;
+
+// console.log(calculation);
+
+// var number = 2;
+
+// var increment = number - ++number + --number - ++number + number-- + number++ - ++number + number - number + number++;
+
+// 2 - 3 + 2 - 3 + 3 + 2 - 4 + 4 - 4 +4
+// console.log(increment);
+
+// var number = 10;
+
+// var increment = number++ - --number - ++number - number++;
+
+// console.log(increment);
+
+// post increment
+
+// number++
+
+// post decrement
+
+// number--
+
+// pre increment
+
+// ++number
+
+// pre increment
+
+// --number
+
+// Table Task
+
+// var number = 9;
+
+// document.write(
+//   number +
+//     " x 1 = " +
+//     number * 1 +
+//     "<br>" +
+//     number +
+//     " x 2 = " +
+//     number * 2 +
+//     "<br>" +
+//     number +
+//     " x 3 = " +
+//     number * 3 +
+//     "<br>" +
+//     number +
+//     " x 4 = " +
+//     number * 4 +
+//     "<br>" +
+//     number +
+//     " x 5 = " +
+//     number * 5 +
+//     "<br>" +
+//     number +
+//     " x 6 = " +
+//     number * 6 +
+//     "<br>" +
+//     number +
+//     " x 7 = " +
+//     number * 7 +
+//     "<br>" +
+//     number +
+//     " x 8 = " +
+//     number * 8 +
+//     "<br>" +
+//     number +
+//     " x 9 = " +
+//     number * 9 +
+//     "<br>" +
+//     number +
+//     " x 10 = " +
+//     number * 10 +
+//     "<br>"
+// );
+
+// alert(12);
+
+// var username = prompt("Enter your name");
+
+// var password = prompt("Enter your password");
+
+// console.log("username", username);
+
+// console.log("password", password);
+
+// var number1 = Number(prompt("Enter first number"));
+// var number2 = Number(prompt("Enter second number"));
+
+// console.log(number1 + number2);
+
+// to convert prompt string into number
+
+// +
+// Number()
+// parseInt()
+
+var number = +prompt("Enter a number to generate a table");
+
+document.write(
+  number +
+    " x 1 = " +
+    number * 1 +
+    "<br>" +
+    number +
+    " x 2 = " +
+    number * 2 +
+    "<br>" +
+    number +
+    " x 3 = " +
+    number * 3 +
+    "<br>" +
+    number +
+    " x 4 = " +
+    number * 4 +
+    "<br>" +
+    number +
+    " x 5 = " +
+    number * 5 +
+    "<br>" +
+    number +
+    " x 6 = " +
+    number * 6 +
+    "<br>" +
+    number +
+    " x 7 = " +
+    number * 7 +
+    "<br>" +
+    number +
+    " x 8 = " +
+    number * 8 +
+    "<br>" +
+    number +
+    " x 9 = " +
+    number * 9 +
+    "<br>" +
+    number +
+    " x 10 = " +
+    number * 10 +
+    "<br>"
+);
