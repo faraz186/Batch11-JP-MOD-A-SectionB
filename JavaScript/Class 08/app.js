2bdd569a9d7c85d1dc870cc7de6dd143ba14b1c3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-20 16:03:18 +0500

    Update app.js

--- JavaScript/Class 08/app.js
+++ JavaScript/Class 08/app.js
@@ -14,9 +14,10 @@ function over(e) {
   console.log(e);
   e.src =
     "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&height=900&width=1600&fit=bounds";
-  var image = document.getElementById("img");
-  image.src =
-    "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&height=900&width=1600&fit=bounds";
+  
+  // var image = document.getElementById("img");
+  // image.src =
+  //   "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&height=900&width=1600&fit=bounds";
 }
 
 // function out(e) {
