aad6157618c4d3ad6dc2b6a24eaa9514fac4ad5a
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-25 20:51:45 +0500

    DOM start 62 CHAPTERS COMPLETE..

---
+++ JavaScript/Class 09/app.js
@@ -0,0 +1,70 @@
+// function over(e) {
+//   //   console.log(e);
+//   e.src =
+//     "https://img.freepik.com/free-photo/cyberpunk-urban-scenery-with-car_23-2150712268.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1713916800&semt=ais";
+// }
+
+// function out(e) {
+//   //   console.log(e);
+//   e.src =
+//     "https://img.freepik.com/free-photo/sports-car-driving-asphalt-road-night-generative-ai_188544-8052.jpg";
+// }
+
+// function changeImage(Eid, ImgSrc) {
+//   var image = document.getElementById(Eid);
+//   console.log(image);
+//   image.src = ImgSrc;
+// }
+
+// function makeInvisible() {
+//   var image = document.getElementById("img");
+//   image.className += " hidden";
+// }
+
+// var paras = document.getElementsByTagName("p");
+
+// console.log(paras);
+
+// var main = document.getElementById("main");
+
+// var paras = main.getElementsByTagName("p");
+
+// console.log(paras);
+
+// for (var i = 0; i < paras.length; i++) {
+//   console.log(paras[i]);
+// }
+
+// var paras = document.getElementsByClassName("para");
+
+// console.log(paras);
+
+// setInterval()
+
+// var interval;
+
+// function timer() {
+//   console.log("function call...");
+// }
+
+// interval = setInterval(function timer() {
+//   console.log("function call...");
+// }, 1000);
+
+// function stop() {
+//   clearInterval(interval);
+// }
+
+// // setTimeout()
+
+// function timer() {
+//   console.log("function call...");
+// }
+
+// setTimeout(timer, 1000);
+
+// console.log(document.childNodes[1].childNodes[1].childNodes);
+
+var main = document.getElementById("main");
+
+console.log(main.childNodes[2].nodeType);
