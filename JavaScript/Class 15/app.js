2452c8e9fa0ab2f1f54943804ce7510618c09d24
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-05-15 00:33:33 +0500

    firebase auth complete..

--- JavaScript/Class 15/app.js
+++ JavaScript/Class 15/app.js
@@ -5,7 +5,7 @@ var firebaseConfig = {
   storageBucket: "**************************",  
   messagingSenderId: "************",  
   appId: "**************************************",
-};
+};  
 
 // Initialize Firebase
 
