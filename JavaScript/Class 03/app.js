371793d98feccc222a2e9c50a479fc6e629b7c49
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-05-22 00:17:50 +0500

    Update app.js

--- JavaScript/Class 03/app.js
+++ JavaScript/Class 03/app.js
@@ -5,7 +5,7 @@ console.log("hello world");
 console.log("hello world");
 console.log("hello world");
 console.log("hello world");
-console.log("hello world");
+console.log("hello world");  
 console.log("hello world");  
 console.log("hello world");     
 console.log("hello world");
